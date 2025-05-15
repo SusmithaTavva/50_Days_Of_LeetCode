d67d54b84ad03fabef74127aa01867f77f835244
SusmithaTavva <162178028+SusmithaTavva@users.noreply.github.com>
2025-05-15 09:39:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (14.26%) - LeetSync

--- 2383-add-two-integers/add-two-integers.java
+++ 2383-add-two-integers/add-two-integers.java
@@ -1,6 +1,8 @@
 class Solution {
     public int sum(int num1, int num2) {
-        int res = num1 + num2;
-        return res;
+        int res = (num1 + num2);
+         return res;
     }
+   
+
 }
